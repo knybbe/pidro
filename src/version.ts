0a89e465865be10ce8ac4d69739ce45abc8ae61e
Kim Nyberg <kim.nyberg@trimble.com>
2026-08-15 11:02:15 +0300

    v0.8.50 - Seamless full-bleed green felt 5 of Hearts logo (70% card height) with zero corner artifacts

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,2 @@
-export const APP_VERSION = 'v0.8.49'
-export const APP_COPYRIGHT = 'Pidro v0.8.49 (C) 2026- Kim Nyberg'
+export const APP_VERSION = 'v0.8.50'
+export const APP_COPYRIGHT = 'Pidro v0.8.50 (C) 2026- Kim Nyberg'
